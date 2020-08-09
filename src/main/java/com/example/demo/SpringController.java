8b5cf3484d7ecb9d041383570bd3edab2925c303
vigne <vigne@LAPTOP-IG98L6AC>
2020-08-09 09:16:46 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class SpringController {
+	
+	@RequestMapping("/hello")
+	public String sayhello() {
+		return "Welcome Vignesh!!!!!";
+	}
+
+}
